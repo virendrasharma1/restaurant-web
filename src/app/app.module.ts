e79fa07582a41d957b73c7f0830ce0887c3b204c
virendrasharma1 <vs82347@gmail.com>
2019-01-01 21:49:36 +0530

    VS - Basic project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { SignInComponent } from './non-authenticated/sign-in/sign-in.component';
-import {RouterModule} from '@angular/router';
-import {routing} from './app.routing';
-import {HttpClientModule} from '@angular/common/http';
-import { RegisterComponent } from './non-authenticated/register/register.component';
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SignInComponent,
-    RegisterComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule,
-    HttpClientModule,
-    routing
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
