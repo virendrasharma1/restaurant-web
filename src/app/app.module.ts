0eb68f878cb403228d40c79d288c3b81f72270a0
virendrasharma1 <vs82347@gmail.com>
2019-01-02 21:41:41 +0530

    VS - Adding jquery and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SignInComponent } from './sign-in/sign-in.component';
+import { SignInComponent } from './non-authenticated/sign-in/sign-in.component';
+import {routing} from './app.routing';
+import { RegisterComponent } from './non-authenticated/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent
+    SignInComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
