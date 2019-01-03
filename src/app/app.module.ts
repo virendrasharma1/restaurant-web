a7ee8ce2e1c83332dd00d47999a19b3205866048
virendrasharma1 <vs82347@gmail.com>
2019-01-03 12:19:17 +0530

    VS - Adding basic pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SignInComponent } from './non-authenticated/sign-in/sign-in.component';
+import { SignInComponent } from './non-authenticated/launch/sign-in/sign-in.component';
 import {routing} from './app.routing';
-import { RegisterComponent } from './non-authenticated/register/register.component';
+import { RegisterComponent } from './non-authenticated/launch/register/register.component';
+import { LaunchComponent } from './non-authenticated/launch/launch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    RegisterComponent
+    RegisterComponent,
+    LaunchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,18 @@
 import {RouterModule, Routes} from '@angular/router';
-import {SignInComponent} from './non-authenticated/sign-in/sign-in.component';
+import {SignInComponent} from './non-authenticated/launch/sign-in/sign-in.component';
+import {LaunchComponent} from './non-authenticated/launch/launch.component';
+import {RegisterComponent} from './non-authenticated/launch/register/register.component';
 
 const appRoutes: Routes = [
-  {path: '', component: SignInComponent}
+  {path: '', redirectTo: '/launch/signin',
+    pathMatch: 'full'},
+  {
+    path: 'launch', component: LaunchComponent,
+    children: [
+      {path: 'signin', component: SignInComponent},
+      {path: 'register', component: RegisterComponent}
+    ]
+  },
 ];
 
 export const routing = RouterModule.forRoot(appRoutes, {useHash: true});
