378820c6d43b9759cbbeb7802e6d6b97831dabfd
virendra.sharma-at-579062553257 <A/rgR+OJgwy1MgRrMhQHeNr99psI0VsvjFztWoVbY1k=>
2019-03-17 17:22:42 +0530

    VS - adding profile

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/restaurant-web'));
+app.use(express.static(__dirname + '/dist/restaurants-web'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join('/dist/restaurant-web/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/restaurants-web/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
